c4b4a8ee3ec262c233ede819a17eec7acb030c54
Daniel Flanagan <danflanman@icloud.com>
2024-11-18 00:14:40 -0500

    Revert back to before last commit

--- app.py
+++ app.py
@@ -210,18 +210,15 @@ def config_options():
             if 'past_chats_selectbox' not in st.session_state:
                 st.session_state['past_chats_selectbox'] = 'Select a prompt'
 
-            if 'last_processed_prompt' not in st.session_state:
-                st.session_state['last_processed_prompt'] = None
-
             if past_prompts:
                 selected_past_prompt = st.sidebar.selectbox(
                     'Past Chats',
                     ['Select a prompt'] + past_prompts,
                     key='past_chats_selectbox'
+                
                 )
-                if (selected_past_prompt and selected_past_prompt != 'Select a prompt' and
-                    selected_past_prompt != st.session_state['last_processed_prompt']):
-                    # Process the prompt
+                if selected_past_prompt and selected_past_prompt != 'Select a prompt':
+                    # Simulate the user entering the prompt
                     st.session_state.messages.append({"role": "user", "content": selected_past_prompt})
                     answer, _ = answer_question(selected_past_prompt)
                     with st.chat_message("user"):
@@ -231,8 +228,6 @@ def config_options():
                     st.session_state.messages.append({"role": "assistant", "content": answer})
                     st.session_state.show_recommendations = False
 
-                    # Update the last processed prompt
-                    st.session_state['last_processed_prompt'] = selected_past_prompt
 
     else:
         st.sidebar.write("Please login to access past chats.")
